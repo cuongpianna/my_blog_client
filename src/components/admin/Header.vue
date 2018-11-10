23934f78917dd196e67b04134b7bb5d0d0dda7e2
Cuong Nguyen <cuongpianna1996@gmail.com>
2018-11-10 12:09:01 +0700

    Fixed Css for Admin page

--- src/components/admin/Header.vue
+++ src/components/admin/Header.vue
@@ -1,3 +1,32 @@
 <template>
-    <div>Header</div>
+    <div class="header-admin">
+        <a href="#" class="admin-logo">
+            <img src="#" alt="" width="89" height="25">
+        </a>
+        <ul class="nav1">
+            <li>
+                <a href="">Dashboard</a>
+            </li>
+            <li>
+                <a href="">User</a>
+            </li>
+            <li>
+                <a href="">Setting</a>
+            </li>
+        </ul>
+    </div>
 </template>
+<style scoped>
+    .admin-logo{
+        width: 200px;
+        height: 100%;
+    }
+    .header-admin{
+        display: flex;
+    }
+
+    .nav1{
+        display: flex;
+        margin-left: 200px;
+    }
+</style>
